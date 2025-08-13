059194871db2aa00c9fc97d6fa71a8c0b59c224b
sinasaaa <129086161+sinasaaa@users.noreply.github.com>
2025-08-13 23:10:05 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+# config.py
+TELEGRAM_TOKEN = "7783938737:AAG0rEmIa0d8g60ISIedZcUbeUijYFQ7guk"
+# اگر میخوای اولیۀ ادمین رو مستقیم بذاری:
+INITIAL_ADMIN_ID = 6133982340  # user_id ادمین اولیه (شما)
+
+# پنل‌ها و API keys در دیتابیس ذخیره میشه؛ ولی یک پیش‌فرض برای تست:
+DEFAULT_PANEL = {
+    "name": "",
+    "base_url": "",
+    "api_key": ""
+}
+
+# تنظیمات عمومی
+DB_PATH = "database.db"
+BOT_NAME = "V2RaySellerBot"
+SERVER_DOMAIN_SUB = "paneltest.bazargahdownloadgame.ir"  # اشاره به ساب آی‌پی ربات (اطلاعات)
